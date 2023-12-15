e136de6c4bedd466b0c5aaa0097569f7b8987f77
PureJD <6454jonny@gmail.com>
2023-12-15 20:20:27 +0000

    general update

--- password.py
+++ password.py
@@ -1,18 +1,25 @@
-loop = True
-while loop == True:
-
-
+while True:
     def password_input():
         pass_input = input('Please enter a password: ')
-        return pass_input 
+        return pass_input
 
     pass_input = password_input()
-    print(pass_input)
-    if len(pass_input) < 8: 
-        print('Please enter a password at least 8')
-        password_input()
-    else: 
+
+    def encrypt(password):
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0','1','2','3','4','5','6','7','8','9']
+        cryptobet = ['t', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0','1','2','3','4','5','6','7','8','9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's']
+        encrypt_password = ''
+        for letter in password:
+            index = 
+        return encrypt_password
+
+        
+
+
+    if len(pass_input) < 8:
+        print('Please enter a password of at least 8 characters')
+    else:
         print('Thank you for the acceptable password')
+        break  
 
-
-
+    encrypt_password = encrypt(pass_input)
