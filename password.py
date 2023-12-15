06ee2e09c467e13d08a0361fb574a6aca50209b4
PureJD <6454jonny@gmail.com>
2023-12-15 16:34:29 +0000

    This is a general update

--- password.py
+++ password.py
@@ -1,6 +1,18 @@
-def password_input():
-    pass_input = input('Please enter a password: ')
-    return pass_input 
+loop = True
+while loop == True:
 
-pass_input = password_input()
-print(pass_input)
+
+    def password_input():
+        pass_input = input('Please enter a password: ')
+        return pass_input 
+
+    pass_input = password_input()
+    print(pass_input)
+    if len(pass_input) < 8: 
+        print('Please enter a password at least 8')
+        password_input()
+    else: 
+        print('Thank you for the acceptable password')
+
+
+
