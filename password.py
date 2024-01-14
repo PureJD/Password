4208a51033c5f57c5fb3ce381a0faf375a3e3972
PureJD <6454jonny@gmail.com>
2024-01-14 15:57:58 +0000

    general update

--- password.py
+++ password.py
@@ -6,15 +6,24 @@ while True:
     
     #Variable to hold the password input
     pass_input = password_input()
+    special_characters = '~`! @#$%^&*()-_+={[]|\;:"<>,./?}'
 
     #Function to check the length of the password is acceptable
     def is_valid_length(pass_input):
         return len(pass_input) < 8
+    
+    def has_special_characters(pass_input):
+        special_char = False
+        for letter in pass_input:
+            if letter in special_characters:
+                special_char == True 
+        return special_char
+            
 
     #Encrypts the password with a very simple encryption. Considerations can be used to convert arrays into a dictionary also. This method allows further encryption by adding further values to indexing.  
     def encrypt(password):
-        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0','1','2','3','4','5','6','7','8','9']
-        cryptobet = ['t', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0','1','2','3','4','5','6','7','8','9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's']
+        alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`! @#$%^&*()-_+={[]|\;:"<>,./?}'
+        cryptobet = 'tuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`! @#$%^&*()-_+={[]|\;:"<>,./?}abcdefghijklmnopqr'
         encrypt_password = ''
         for letter in password:
             index = alphabet.index(letter) 
@@ -24,6 +33,9 @@ while True:
         
     if is_valid_length(pass_input):
         print('Please enter a password of at least 8 characters')
+    elif has_special_characters(pass_input):
+        
+        print('Please enter a password with at least one of the following special characters ~`! @#$%^&*()-_+={[]|\;:"<>,./?}'')')
     else:
         print('Thank you for the acceptable password') 
         encrypt_password = encrypt(pass_input)
