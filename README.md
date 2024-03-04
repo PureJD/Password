0bb1aa9419ebbb0818745d7437cd44b21c71760c
PureJD <6454jonny@gmail.com>
2024-03-04 14:38:54 +0000

    This is an update to the program adding a database element

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Password
-A password app which requires specific input, encrypts and can suggest passwords.
-The git repository has been created at the earliest oppertunity in order to progress this project over the course of multiple sessions. The code at this stage does not represent the above description. 
+# Password encrypter and database
+### This is a program that allows you to add a simple password and it will generate a high level encryption version of that password which will then be stored and can be retrieved by the user at any time. 
+![Encrypt](https://www.hideipvpn.com/wp-content/uploads/2022/11/Python_Fernet.jpg)
+
+
+### - The code performs the following steps:
+- Loads SQL and encryption Fernet encryption libraries
+- Loads the SQL database
+- Prompts the user to enter a basic password within limitations
+- Uses fernet encryption to create a complex password
+- Saves the password in the database as its encrypted form
+- Allows the user to retrieve their password 
+
+
+## This program is a work in progress. Clear security issues are known in regards to accessing the database to obtain ones passwords.
+
+
+![GitHub Logo](https://github.com/github.png)
--- password.py
+++ password.py
@@ -1,79 +1,113 @@
 from cryptography.fernet import Fernet
 import sys
-while True:
-    
-    #Captures the password input from the user 
-    def password_input():
-        pass_input = input('Please enter a password: ')
-        return pass_input
-    
-    #Variable to hold the password input
-    pass_input = password_input()
-    special_characters = '~`! @#$%^&*()-_+={[]|\;:"<>,./?}'
+import os
+import sqlite3
 
-    #Function to check the length of the password is acceptable
-    def is_valid_length(pass_input):
-        return len(pass_input) < 8
-    
-    def has_special_characters(pass_input):
+special_characters = '~`! @#$%^&*()-_+={[]|\;:"<>,./?}'
+conn = sqlite3.connect('passwords.db')
+cursor = conn.cursor()  # Create a cursor object
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    encrypted_password BLOB
+                  )''')
+
+def insert_user(name_of_service, harder_pass):
+    cursor.execute('INSERT INTO users (username, encrypted_password) VALUES (?, ?)', (name_of_service, harder_pass))
+    conn.commit()
+
+def clearingline():
+    os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console 
+
+def service_name(): #Captures the service name input from the user
+    service_name = input('\033[92mPlease enter the name of the service you wish to create and save a password for: \033[0m')
+    clearingline()
+    return service_name
+
+def password_input(): #Captures the password input from the user 
+    pass_input = input('\033[92mPlease enter a password: \033[0m')
+    clearingline()
+    return pass_input
+
+def is_valid_length(pass_input): #Function to check the length of the password is acceptable
+    return len(pass_input) < 8
+
+def has_special_characters(pass_input):
         special_char = True
         for letter in pass_input:
             if letter in special_characters:
                 special_char = False
                 break
         return special_char
-            
-    #Encrypts the password with a very simple encryption. Considerations can be used to convert arrays into a dictionary also. This method allows further encryption by adding further values to indexing.  
-    def encrypt(password):
-        alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`! @#$%^&*()-_+={[]|\;:"<>,./?}'
-        cryptobet = 'tuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`! @#$%^&*()-_+={[]|\;:"<>,./?}abcdefghijklmnopqr'
-        encrypt_password = ''
-        for letter in password:
-            index = alphabet.index(letter) 
-            encrypt_password += cryptobet[index]
-        return encrypt_password
 
-    def harder_encryption(pass_input):
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-        encMessage = fernet.encrypt(pass_input.encode())
-        decMessage = fernet.decrypt(encMessage).decode()
-        return encMessage
+def harder_encryption(pass_input): #Function to encrypt the password
+    key = Fernet.generate_key()
+    fernet = Fernet(key) 
+    encMessage = fernet.encrypt(pass_input.encode())
+    decMessage = fernet.decrypt(encMessage).decode()
+    return encMessage
+
+def harder_deencryption(harder_pass):
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    encMessage = fernet.encrypt(pass_input.encode())
+    decMessage = fernet.decrypt(encMessage).decode()
+    return decMessage
+
+def menu():
+    chosen_path = input('''\033[92mPlease select where you would like to go next:\033[0m 
+                        \033[94m1. ADD SERVICE PASSWORD\033[0m  
+                        \033[94m2. SHOW ALL SAVED PASSWORDS\033[0m  
+                        \033[94m3. INFO\033[0m  
+                        \033[94m4. EXIT\033[0m 
+: ''')
+    return chosen_path
     
-    def harder_deencryption(harder_pass):
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-        encMessage = fernet.encrypt(pass_input.encode())
-        decMessage = fernet.decrypt(encMessage).decode()
-        return decMessage
+def exit():
+    sys.exit()
 
+def show_passwords():
+    cursor.execute('SELECT * FROM users')
+    for row in cursor.fetchall():
+        print(row)
 
-    def menu():
-        chosen_path = input('Please select where you would like to go next: 1. ANOTHER PASSWORD  2. HARDER ENCRYPTION  3. INFO  4. EXIT  :')
-        return chosen_path
+clearingline()
+
+while True:
+    #This section of the code requests which service the password is for and the password they wish to use.
+    name_of_service = service_name()
+    pass_input = password_input()
+   
     
-    def exit():
-        sys.exit()
-
         
     if is_valid_length(pass_input):
-        print('Please enter a password of at least 8 characters')
+        print('\033[92mPlease enter a password of at least 8 characters\033[0m')
     elif has_special_characters(pass_input):
-        print('Please enter a password with at least one of the following special characters ~`! @#$%^&*()-_+={[]|\;:"<>,./?}'')')
+        print('\033[92mPlease enter a password with at least one of the following special characters ~`! @#$%^&*()-_+={[]|\;:"<>,./?}'')\033[0m')
     else:
-        print('Thank you for the acceptable password') 
-        encrypted_password = encrypt(pass_input)
-        print('Your password has now been ecrypted as ' + encrypted_password)
+        print('\033[92mThank you for the acceptable password\033[0m') 
+        encrypted_password = harder_encryption(pass_input)
+        print(f'\033[92mYour password has now been ecrypted as: \033[0m')
+        print(f'\033[95m{encrypted_password}\033[0m')
+
+        insert_user(name_of_service, encrypted_password) #Inserts the service name and password into the database
+
         chosen_path = menu()
+
         if chosen_path == '4':
+            conn.close()
             exit()
         elif chosen_path == '3':
-            print('Password protection is important in order to protect your personal data. There is no such thing as ''Encrypted'' and ''non-Encrypted'' as there are levels to encryption that are constantly being improved upon. One major fear in the computer science community it that quantum computing is going to be able to decrypt anything with little effort or time.')
+            clearingline()
+            print('''\033[94mPassword protection is important in order to protect your personal data. There is no such thing as ''Encrypted'' and ''non-Encrypted'' as there are levels to encryption that are constantly being improved upon. One major fear in the computer science community it that quantum computing is going to be able to decrypt anything with little effort or time.\033[0m''')
         elif chosen_path == '2':
-            harder_pass = harder_encryption(pass_input)
-            print(f'This is an example of a higher level of ecryption{harder_pass}')
-            harder_pass_undo = harder_deencryption(harder_pass)
-            print(f'The above represents a high level encryption of password : {harder_pass_undo}')
+            clearingline()
+            show_passwords()
+        elif chosen_path == '1':    
+            clearingline()
+            name_of_service = service_name()
+    pass_input = password_input()
+    clearingline()
         
 
 
